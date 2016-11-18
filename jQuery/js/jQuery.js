7612147699e16f0b877a85007ac6fa62d8c7a029
phillipsK <kjphillips@suffolk.edu>
2016-11-18 17:03:53 -0500

    completed part 1 of Assignment 3

--- jQuery/js/jQuery.js
+++ jQuery/js/jQuery.js
@@ -2,82 +2,76 @@
  * jQuery assignment
  * */
 $(document).ready(function() {
-    /*    $('#mobile').change(function() {
-     // this will contain a reference to the checkbox
-     if (this.checked) {
-     // the checkbox is now checked
-     alert("hi");
-     } else {
-     // the checkbox is now no longer checked
-     }
-     });*/
-
-    /*    $('#mobile').change(function() {
-     if($(this).is(":checked")) {
-     var returnVal = confirm("Are you sure?");
-     $(this).attr("checked", returnVal);
-     }
-     });*/
 
     $('#mobile').hide();
 
-    $('#checkedYes').change(function(){
+    $('#checkedYes').change(function () {
         if ($('#checkedYes').is(":checked")) {
             //alert("hi");
             $('#mobile').show();
-                $("#phone").hover(function(){
-                    $(this).css("background-color", "yellow");
-                }, function(){
-                    $(this).css("background-color", "lightcoral");
-                });
-        }else if ($('#checkedYes').not(":checked")){
+            $("#phone").hover(function () {
+                $(this).css("background-color", "yellow");
+            }, function () {
+                $(this).css("background-color", "lightcoral");
+            });
+        } else if ($('#checkedYes').not(":checked")) {
             $('#mobile').hide();
         }
     });
 
+
+    /* My second form not related to the "grading form" I merely used and additional form
+     * here to convey my knowledge of jQuery
+     * */
+
     $('#prayer_phone').hide();
     $('#prayer_person').hide();
     $('#prayer_church').hide();
-/*    $('#emails').hide();
-    $('#submit').hide();*/
+    $('#maincontent p:first').removeClass('addColor');
 
 
-    $( "#phone_target" ).click(function() {
+    $("#phone_target").click(function () {
         $('#prayer_phone').show();
+        $('#maincontent p:first').addClass('addColor');
         $('#prayer_person').hide();
         $('#prayer_church').hide();
     });
-    $( "#In_Person_target" ).click(function() {
+    $("#In_Person_target").click(function () {
         $('#prayer_person').show();
+        $('#prayer_person').css('font-size', '.7em');
+        $('#prayer_person').css('font-family', 'Droid Arabic Naskh');
+        $('#maincontent p:first').removeClass('addColor');
         $('#prayer_phone').hide();
         $('#prayer_church').hide();
     });
-    $( "#At_Church_target" ).click(function() {
-        $('#prayer_church').show();
-        $('#prayer_phone').hide();
-        $('#prayer_person').hide();
+    $("#At_Church_target").hover(function () {
+            $('#prayer_church').show();
+            $('#maincontent p:first').removeClass('addColor');
+            $('#prayer_person').hide();
+            $('#prayer_phone').hide();
+            $('#At_Church_target').css('color', 'blue');
+            $('#prayer_church').css('color', 'green');
+        },
+        function () {
+            $('#prayer_church').hide();
+            $('#At_Church_target').css('color', 'black');
+        });
+
+/* My own events */
+
+    $('#Location_preferences').change(function () {
+        if ($('#Location_preferences').val() == 'library') {
+            $('li:even').css('font-size','2em');
+        } else if ($('#Location_preferences').val() == 'college') {
+            $('li:odd').css('font-size','2em');
+            $('li:even').css('font-size','1em');
+        } else if ($('#Location_preferences').val() == 'home') {
+            $('li:odd').css('text-transform','uppercase');
+        } else if ($('#Location_preferences').val() == 'other') {
+            $('li:even').css('font-size','1em');
+        } else {
+        }
     });
 
 
-
-
-
-    /*    $('#prayer_requests Phone').each(function() {
-            if($(this).is(':selected'))*/
-
- /*           $('#prayer_requests').change(function(){
-                if ($('#prayer_requests').val() == 'Phone') {
-                    // $('.msg').html('Apples are a great seasonal choice!');
-                    $('#phone_details').show();
-                } else if ($('#prayer_requests').val() == 'Email') {
-                    // $('.msg').html('Blueberries are my favorite!');
-                } else if ($('#prayer_requests').val() == 'In-Person') {
-                    // $('.msg').html('Our cherries are fresh, not from a can.');
-                } else if ($('#prayer_requests').val() == 'At Church') {
-                    // $('.msg').html('Happy Thanksgiving!');
-                } else {
-                    // $('.msg').html('Never heard of that filling. Better call and see if we have it.');
-                }
-            }); //end change
-*/
 });
