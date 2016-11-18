2bd41153ef915de20b22b7d7d86275512e9023ed
phillipsK <kjphillips@suffolk.edu>
2016-11-17 22:56:35 -0500

    click events completed

--- jQuery/js/jQuery.js
+++ jQuery/js/jQuery.js
@@ -35,4 +35,49 @@ $(document).ready(function() {
         }
     });
 
+    $('#prayer_phone').hide();
+    $('#prayer_person').hide();
+    $('#prayer_church').hide();
+/*    $('#emails').hide();
+    $('#submit').hide();*/
+
+
+    $( "#phone_target" ).click(function() {
+        $('#prayer_phone').show();
+        $('#prayer_person').hide();
+        $('#prayer_church').hide();
+    });
+    $( "#In_Person_target" ).click(function() {
+        $('#prayer_person').show();
+        $('#prayer_phone').hide();
+        $('#prayer_church').hide();
+    });
+    $( "#At_Church_target" ).click(function() {
+        $('#prayer_church').show();
+        $('#prayer_phone').hide();
+        $('#prayer_person').hide();
+    });
+
+
+
+
+
+    /*    $('#prayer_requests Phone').each(function() {
+            if($(this).is(':selected'))*/
+
+ /*           $('#prayer_requests').change(function(){
+                if ($('#prayer_requests').val() == 'Phone') {
+                    // $('.msg').html('Apples are a great seasonal choice!');
+                    $('#phone_details').show();
+                } else if ($('#prayer_requests').val() == 'Email') {
+                    // $('.msg').html('Blueberries are my favorite!');
+                } else if ($('#prayer_requests').val() == 'In-Person') {
+                    // $('.msg').html('Our cherries are fresh, not from a can.');
+                } else if ($('#prayer_requests').val() == 'At Church') {
+                    // $('.msg').html('Happy Thanksgiving!');
+                } else {
+                    // $('.msg').html('Never heard of that filling. Better call and see if we have it.');
+                }
+            }); //end change
+*/
 });
